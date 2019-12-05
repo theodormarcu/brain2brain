98e8cdc0f48b5ceddbc4efc2b2b400532ddb1666
Theodor Marcu <marcu.theodor@gmail.com>
2019-12-05 17:16:30 -0500

    Testing.

---
+++ utils/strain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time 1:00:00
+#SBATCH --mem=80GB
+#SBATCH -N 1
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-socket=1
+#SBATCH --gres=gpu:1
+#SBATCH -o 'training-%A.log'
+
+# You can use `salloc --gres=gpu:1 --time 01:10:00` to test something
+
+module load anaconda3
+#module load cudatoolkit/9.2
+#module load cudnn/cuda-9.2
+conda activate tiger_gpu_env
+
+echo 'Start time:' `date`
+echo "$@"
+# python bash_style_train_tcn.py "$@"
+# python get_data.py "$@"
+echo 'End time:' `date`
