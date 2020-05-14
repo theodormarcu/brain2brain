ca1066dc4126d7329d052121392fce1ccadca60a
Theodor Marcu <marcu.theodor@gmail.com>
2020-05-14 00:57:05 -0400

    added trainer/tester packages

---
+++ brain2brain/trainer/__main__.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Theodor Marcu
+# tmarcu@princeton.edu
+# Created January 2020
+# Computer Science Senior Thesis
+'''
+Main class for CLI functionality.
+'''
+
+import os
+import importlib
+import importlib.util
+import argparse
+from brain2brain import utils
+from brain2brain import generators
+from brain2brain import tcn_experiments, baseline_experiments, gru_experiments, optimization
+import numpy as np
+import pprint
+import yaml
+
+
+def main():
+    '''
+    Main function for __main__.py. This is the entry point in the CLI.
+    '''
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+    parser.add_argument("experiment_folder", 
+                        help="Path to experiment folder that contains a YAML file and model.py.", type=str)
+    args = parser.parse_args()
+
+    params_file_name = "params.yaml"
+    params_model_file_name = "model.py"
+    
+    # Read YAML Experiment File
+    with open(os.path.join(args.experiment_folder, params_file_name)) as f:
+        experiment_params = yaml.load(f, Loader=yaml.SafeLoader)
+
+    model_full_path = os.path.join(args.experiment_folder, params_model_file_name)
+
+    # Load Model From File
+    spec = importlib.util.spec_from_file_location("model", model_full_path)
+    model = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(model)
+
+    model.train(experiment_params)
+
+if __name__ == '__main__':
+    main()
---
+++ experiments/test_experiment/model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Theodor Marcu
+# tmarcu@princeton.edu
+# Created May 2020
+# Computer Science Senior Thesis
+
+def train(experiment_params):
+    """
+    Model definiton goes here. Add paramters as you see fit.
+    """
+
+    print("Training Model!")
+    print(experiment_params)
